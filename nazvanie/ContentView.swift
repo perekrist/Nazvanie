a2e6164d2ee2c48dbe798109825f1372218b6096
perekrist <perekrist31@gmail.com>
2020-06-06 11:38:28 +0700

    Initial Commit

---
+++ nazvanie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nazvanie
+//
+//  Created by Кристина Перегудова on 06.06.2020.
+//  Copyright © 2020 Кристина Перегудова. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
